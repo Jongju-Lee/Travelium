b520c733291884171801ac302c6f3b9f31cf7f86
jongju-Lee <jeje7@naver.com>
2024-04-18 02:41:57 +0900

    edit responsive

--- js/custom.js
+++ js/custom.js
@@ -1,11 +1,23 @@
 $(function () {
+  /* Header Scroll Events */
+  $(window).scroll(function () {
+    if (window.scrollY > 50) {
+      // 스크롤 하면
+      $("header").addClass("active");
+    } else if (window.scrollY < 50) {
+      $("header").removeClass("active");
+    }
+  });
   /* ##### Slick.js ##### */
   $(".popular_slider").slick({
     dots: true,
+    arrows: false,
     infinite: true,
     speed: 300,
     slidesToShow: 5,
     slidesToScroll: 5,
+    autoplay: true,
+    autoplaySpeed: 5000,
     responsive: [
       {
         breakpoint: 1366,
@@ -17,6 +29,31 @@ $(function () {
       {
         breakpoint: 480,
         settings: {
+          autoplay: true,
+          autoplaySpeed: 5000,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  });
+  $(".special_slider").slick({
+    centerMode: true,
+    centerPadding: "200px",
+    arrows: true,
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: false,
+          arrows: false,
           slidesToShow: 1,
           slidesToScroll: 1,
         },
